22701065481531196957cd530afeffc6d1ae686e
An Hyang Bin <mic102122@naver.com>
2020-10-29 23:32:52 +0900

    메인화면에 Container요소로 구역 나눔

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
+import EquationList from './components/EquationList';
 import './App.css';
 import BottomToolbarBox from './BottomToolbarBox';
+import {Container} from '@material-ui/core';
 
 class App extends Component{
   render(){
     return (
-      <BottomToolbarBox>abc</BottomToolbarBox>
+      <>
+      <Container>
+      <EquationList></EquationList>
+      </Container>
+      <Container>
+      <BottomToolbarBox></BottomToolbarBox>
+      </Container>
+      </>
     );
   }
 }
+
 export default App;
