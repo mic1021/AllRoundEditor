4398755dcf335a70ab5231208b8fce43b2cfbf9a
An Hyang Bin <mic102122@naver.com>
2020-11-02 00:53:53 +0900

    Component 표기방식을 class에서 functional로 변경
    1. 기존 LifeCycle관련 메소드가 하던 기능(componentDidMount -> shouldComponentUpdate -> componentWillunmount)을 useEffect()로 변경
    2. constructor() 대신 useState구문 사용
    3. shouldComponentUpdate에서 반환하던 조건절을 handleResize()에서 setWindowSize()를 호출하는 조건절로 설정

--- frontend/src/components/buttonGridComponents/BottomToolbarBox.js
+++ frontend/src/components/buttonGridComponents/BottomToolbarBox.js
@@ -1,48 +1,37 @@
-import React,{ Component , useState} from 'react';
+  import React,{useState, useEffect} from 'react';
 import BottomToolbarRow from './BottomToolbarRow';
 import { Grid} from '@material-ui/core';
 import {addStyles, StaticMathField, EditableMathField } from 'react-mathquill'
 
 addStyles();
 
-class BottomToolbarBox extends Component{
-  constructor() {
-    super();
-    this.state = {
-      WindowSize : window.innerWidth
+const BottomToolbarBox = (props) => {
+  const [WindowSize,setWindowSize] = useState(window.innerWidth);
+  useEffect(()=>{
+    window.addEventListener("resize", handleResize);
+  },);
+  const handleResize = (event) => {
+    if((WindowSize<600 && window.innerWidth>=600)
+    ||(WindowSize>=600 && window.innerWidth<600)
+    ||(WindowSize<960 && window.innerWidth>=960)
+    ||(WindowSize>=960 && window.innerWidth<960)
+    ||(WindowSize<1280 && window.innerWidth>=1280)
+    ||(WindowSize>=1280 && window.innerWidth<1280)){
+      setWindowSize(window.innerWidth);
     }
-    this.handleResize = this.handleResize.bind(this);
-  }
-  componentDidMount() {
-    window.addEventListener("resize", this.handleResize);
-  }
-  componentWillUnmount() {
-    window.addEventListener("resize", null);
-  }
-  handleResize(WindowSize, event) {
-    this.setState({WindowSize: window.innerWidth})
-  }
-  shouldComponentUpdate(nextProps,nextState){
-    return ((nextState.WindowSize<600 && this.state.WindowSize>=600)
-    ||(nextState.WindowSize>=600 && this.state.WindowSize<600)
-    ||(nextState.WindowSize<960 && this.state.WindowSize>=960)
-    ||(nextState.WindowSize>=960 && this.state.WindowSize<960)
-    ||(nextState.WindowSize<1280 && this.state.WindowSize>=1280)
-    ||(nextState.WindowSize>=1280 && this.state.WindowSize<1280));
-  }
+  };
 
-  getMathSymbol(_latex, _fontSize = '300%') {
+  const getMathSymbol = (_latex, _fontSize = '300%') => {
     return <><StaticMathField style={{ fontSize: _fontSize }}>{_latex}</StaticMathField><div style={{ position: 'absolute', top: '80%' }}>{_latex}</div></>
-  }
-  render() {
-    const row = [this.getMathSymbol('\\frac{}{}', '200%'), this.getMathSymbol('\\sqrt{}'), this.getMathSymbol('\\sqrt[]{}'), this.getMathSymbol('\\int', '170%'), this.getMathSymbol('\\sum', '130%'), this.getMathSymbol('+', '400%'), this.getMathSymbol('-', '400%'), this.getMathSymbol('\\times', '400%'), this.getMathSymbol('\\div', '400%'), this.getMathSymbol('\\pm', '400%'), this.getMathSymbol('\\mp'), this.getMathSymbol('\\sin^{}')];
-    const latexs = ['\\sqrt{}', '\\sqrt[]{}', '\\frac{}{}', '+', '-', '\\times', '\\div', '\\pm', '\\mp', '\\int', '\\sum', '\\sin^{}', '\\cos^{}', '\\tan^{}', '', '', '', '', '', '', '', ''];
-    return (
-      <Grid container spacing={1}>
-        <BottomToolbarRow row={row}></BottomToolbarRow>
-      </Grid>
-    );
-  }
+  };
+
+  const row = [getMathSymbol('\\frac{}{}', '200%'), getMathSymbol('\\sqrt{}'), getMathSymbol('\\sqrt[]{}'), getMathSymbol('\\int', '170%'), getMathSymbol('\\sum', '130%'), getMathSymbol('+', '400%'), getMathSymbol('-', '400%'), getMathSymbol('\\times', '400%'), getMathSymbol('\\div', '400%'), getMathSymbol('\\pm', '400%'), getMathSymbol('\\mp'), getMathSymbol('\\sin^{}')];
+  const latexs = ['\\sqrt{}', '\\sqrt[]{}', '\\frac{}{}', '+', '-', '\\times', '\\div', '\\pm', '\\mp', '\\int', '\\sum', '\\sin^{}', '\\cos^{}', '\\tan^{}', '', '', '', '', '', '', '', ''];
+  return (
+    <Grid container spacing={1}>
+      <BottomToolbarRow row={row}></BottomToolbarRow>
+    </Grid>
+  );
 }
 
 export default BottomToolbarBox;
