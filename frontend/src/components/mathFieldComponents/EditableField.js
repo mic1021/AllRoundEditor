2d2ffa1e06fc11608895896c9ad93f9bcee47746
An Hyang Bin <mic102122@naver.com>
2020-11-10 17:28:40 +0900

    사용자가 모달창에서 수식 선택 시 모달창을 띄우고 마우스나 키보드로 커서를 움직일 때마다 redux에 커서위치를 갱신
    1. 사용자가 모달창에서 수식 선택 시 모달창을 띄움
    2. 마우스나 키보드로 커서를 움직일 때마다 redux에 커서위치를 갱신

--- frontend/src/components/mathFieldComponents/EditableField.js
+++ frontend/src/components/mathFieldComponents/EditableField.js
@@ -11,7 +11,8 @@ const useStyles = makeStyles({
     },
 });
 
-let modifying=false,firstWhileFinished=false,secondWhileFinished=false,selectedLatex="",binaryOperator=false,written=false,deleteCnt=0,cur=0;
+//let modifying = false, firstWhileFinished = false, secondWhileFinished = false, binaryOperator = false, written = false, deleteCnt = 0, cur = 0;
+let written = false, deleteCnt = 0, cur = 0;
 let localMathField;
 
 function EditableField(props) {
@@ -27,108 +28,107 @@ function EditableField(props) {
         }
     }, [showDialogue, mathCmd]);
 
-    const showAutoCompleteAndSelect = (latexField) => {
-      //\로 시작하면 해당 글자에 해당하는 수식 후보군을 모달창에 보여주고, 사용자가 선택한 수식을 반환
-      //사용자가 선택하지 않았을 경우(esc로 종료한 경우) 빈문자열을 반환
-      dispatch(toggleDialogue(''));
-
-      //return '\\frac';
-    }
-
     const handleChange = (mathField) => {
         localMathField = mathField;
         // Called everytime the input changes
         let nowCursor = editableField.current.getElementsByClassName('mq-hasCursor')[0];
-        if(nowCursor !== undefined){
-            if(modifying===false && nowCursor.innerText[0] === "\\"){
-                selectedLatex=showAutoCompleteAndSelect(nowCursor.innerText);
-            }   
-            if(selectedLatex!==""){
-                if(modifying===false && isCommandInput(nowCursor)){
-                    modifying=true;
-                    mathField.cmd(selectedLatex);
-                    modifying=false;
-                    firstWhileFinished=false;
-                    secondWhileFinished=false;
-                    if(binaryOperator){
-                        mathField.keystroke('Right');
-                        binaryOperator=false;
-                    }
-                    selectedLatex="";
-                    dispatch(TYPE(mathField.latex()));
-                }
-                else if(modifying===true){
-                    let nowCursorElement = editableField.current.getElementsByClassName('mq-cursor')[0];
-                    while(!firstWhileFinished){
-                        if(isBinaryOperator(nowCursorElement.nextSibling) || isMqnonleaf(nowCursorElement.nextSibling)){
-                            firstWhileFinished=true;
-                            break;
-                        }
-                        mathField.keystroke('Left');
-                        nowCursorElement = editableField.current.getElementsByClassName('mq-cursor')[0];
-                    }
-                    while(!secondWhileFinished){
-                        mathField.keystroke('Left');
-                        nowCursorElement = editableField.current.getElementsByClassName('mq-cursor')[0];
-                        if(isCommandInput(nowCursorElement.nextSibling)) {
-                            secondWhileFinished=true;
-                        }
-                        mathField.keystroke('Right');
-                        mathField.keystroke('Backspace');
-                        break;
-                    }
-                }
+        if (nowCursor !== undefined) {
+            //if(selectedLatex!==""){
+            //     if(modifying===false && isCommandInput(nowCursor)){
+            //         modifying=true;
+            //         mathField.cmd(selectedLatex);
+            //         modifying=false;
+            //         firstWhileFinished=false;
+            //         secondWhileFinished=false;
+            //         if(binaryOperator){
+            //             mathField.keystroke('Right');
+            //             binaryOperator=false;
+            //         }
+            //         selectedLatex="";
+            //         dispatch(TYPE(mathField.latex()));
+            //     }
+            //     else if(modifying===true){
+            //         let nowCursorElement = editableField.current.getElementsByClassName('mq-cursor')[0];
+            //         while(!firstWhileFinished){
+            //             if(isBinaryOperator(nowCursorElement.nextSibling) || isMqnonleaf(nowCursorElement.nextSibling)){
+            //                 firstWhileFinished=true;
+            //                 break;
+            //             }
+            //             mathField.keystroke('Left');
+            //             nowCursorElement = editableField.current.getElementsByClassName('mq-cursor')[0];
+            //         }
+            //         while(!secondWhileFinished){
+            //             mathField.keystroke('Left');
+            //             nowCursorElement = editableField.current.getElementsByClassName('mq-cursor')[0];
+            //             if(isCommandInput(nowCursorElement.nextSibling)) {
+            //                 secondWhileFinished=true;
+            //             }
+            //             mathField.keystroke('Right');
+            //             mathField.keystroke('Backspace');
+            //             break;
+            //         }
+            //     }
+            //}
+            //else {
+            if (!written) {
+                written = true;
+                mathField.write('!@#');
             }
             else {
-                if(!written){
-                    written=true;
-                    mathField.write('!@#');
+                if (deleteCnt < 3) {
+                    if (deleteCnt === 0) {
+                        cur = mathField.latex().indexOf("!@#");
+                    }
+                    deleteCnt++;
+                    mathField.keystroke('Backspace');
                 }
-                else{
-                    if(deleteCnt<3){
-                        if(deleteCnt===0) {
-                            cur = mathField.latex().indexOf("!@#");
-                        }
-                        deleteCnt++;
-                        mathField.keystroke('Backspace');
-                    }
-                    else{
-                        written=false;
-                        deleteCnt=0;
-                        dispatch(CURSOR(cur));
-                        dispatch(TYPE(mathField.latex()));
-                    }
+                else {
+                    written = false;
+                    deleteCnt = 0;
+
+                    dispatch(CURSOR(cur));
+                    dispatch(TYPE(mathField.latex()));
                 }
             }
+            //}
         }
     }
 
     const updateCursorPosition = (e) => {
-        if(localMathField!==undefined) {
+        if (localMathField !== undefined) {
             handleChange(localMathField);
+        }
+    }
+
+    const handleKeyDown = (e) => {
+        if (e.keyCode == '37' || e.keyCode == '39') { // left right
+            handleChange(localMathField);
+        } else if (e.keyCode == '220') {     // backslash
+            dispatch(toggleDialogue(''));
         }
     }
 
     return (
         <div ref={editableField}>
-          <EditableMathField
-            latex={latex}
-            onChange={handleChange}
-            onClick={updateCursorPosition}
-          />
-          {showDialogue && <EquationSuggestionModal text={selectedLatex}></EquationSuggestionModal>}
+            <EditableMathField
+                latex={latex}
+                onChange={handleChange}
+                onClick={updateCursorPosition}
+                onKeyDown={handleKeyDown}
+            />
+            {showDialogue && <EquationSuggestionModal></EquationSuggestionModal>}
         </div>
     )
 }
-const isCommandInput = (nowCursor) => {
-    return nowCursor!==null && nowCursor.className.includes("mq-latex-command-input");
-}
-const isMqnonleaf = (nowCursor) => {
-    return nowCursor!==null && nowCursor.className.includes("mq-non-leaf");
-}
+// const isCommandInput = (nowCursor) => {
+//     return nowCursor !== null && nowCursor.className.includes("mq-latex-command-input");
+// }
+// const isMqnonleaf = (nowCursor) => {
+//     return nowCursor !== null && nowCursor.className.includes("mq-non-leaf");
+// }
 
-const isBinaryOperator = (nowCursor) => {
-    return binaryOperator = (nowCursor!==null && nowCursor.className.includes("mq-binary-operator"));
-}
+// const isBinaryOperator = (nowCursor) => {
+//     return binaryOperator = (nowCursor !== null && nowCursor.className.includes("mq-binary-operator"));
+// }
 
 export default EditableField
