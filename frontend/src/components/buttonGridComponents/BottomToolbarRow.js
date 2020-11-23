705dfa3a0fd3b1bfccf1706d4a1362248c3676ea
An Hyang Bin <mic102122@naver.com>
2020-11-24 00:31:37 +0900

    수식툴바 부분을 material-ui의 tab 컴포넌트로 교체하고 디자인 수정

--- frontend/src/components/buttonGridComponents/BottomToolbarRow.js
+++ frontend/src/components/buttonGridComponents/BottomToolbarRow.js
@@ -1,39 +1,44 @@
 import React from 'react';
-import { Button , Grid} from '@material-ui/core';
-import { withStyles } from '@material-ui/core/styles';
+import { Button , Grid , AppBar, Tab, Tabs} from '@material-ui/core';
 import { useSelector, useDispatch } from 'react-redux';
 import { selectCursor, selectLatex, TYPE } from '../../slices/EquationSlice';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core';
 
-const styles = {
+const useStyles = makeStyles((theme) => ({
   root:{
-    background: 'skyblue'
+    background: '#3f51b5'
     ,color:'black'
   },
   button:{
     maxWidth:'1000px'
     ,maxHeight:'1000px'
-    ,minWidth:'10px'
+    ,minWidth:'90px'
     ,minHeight:'100px'
     ,background: 'white'
     ,textTransform: 'lowercase'
-  }
-};
+  },
+  root2: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+})); 
 
-function BottomToolbarRow(props){
-
+function TabPanel(props) {
   let currentLatex = useSelector(selectLatex);
   let currentCursorPosition = useSelector(selectCursor);
+  const { children, value, index, ...other } = props;
+  const classes = useStyles();
   const dispatch = useDispatch();
   const getItemJSX = (items) => {
-    const {classes} = props;
     return items.map(
-      (item) => {
-        return <Grid item className = {classes.root} xs={3} sm={2} md={2} lg={1} xl={1}>
+      (item,index) => {
+        return <Grid key={index} item className = {classes.root} xs={3} sm={2} md={2} lg={1} xl={1}>
           <Button className={classes.button} 
             onClick={buttonClickEvent(item,currentCursorPosition,currentLatex)} fullWidth>
             {item}
           </Button>
-          </Grid>
+        </Grid>
       }
     );
   }
@@ -45,10 +50,64 @@ function BottomToolbarRow(props){
 
   const items = getItemJSX(props.row);
 
-  return(
-    <>
-      {items}
-    </>
-  )
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      <Grid container spacing={1}>
+        {items}
+      </Grid>
+    </div>
+  );
 }
-export default withStyles(styles)(BottomToolbarRow);
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  alue: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+function BottomToolbarRow(props){
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return(
+    <div className={classes.root2}>
+      <AppBar position="static">
+        <Tabs 
+          value={value}
+          onChange={handleChange}
+          aria-label="simple tabs example"
+        >
+          <Tab label="Main" {...a11yProps(0)} />
+          <Tab label="Other" {...a11yProps(1)} />
+          <Tab label="Arithmetic" {...a11yProps(2)} />
+          <Tab label="Logic" {...a11yProps(3)} />
+          <Tab label="Order" {...a11yProps(4)} />
+          <Tab label="Set" {...a11yProps(5)} />
+        </Tabs>
+      </AppBar>
+      <TabPanel row = {props.row[0]} value={value} index={0}> </TabPanel>
+      <TabPanel row = {props.row[1]} value={value} index={1}> </TabPanel>
+      <TabPanel row = {props.row[2]} value={value} index={2}> </TabPanel>
+      <TabPanel row = {props.row[3]} value={value} index={3}> </TabPanel>
+      <TabPanel row = {props.row[4]} value={value} index={4}> </TabPanel>
+      <TabPanel row = {props.row[5]} value={value} index={5}> </TabPanel>
+    </div>
+  );
+}
+export default BottomToolbarRow;
