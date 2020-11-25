1b245677b7bca0305ddbee6468d132134559df78
An Hyang Bin <mic102122@naver.com>
2020-11-26 00:13:45 +0900

    App.css 적용하도록 수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Demo from './components/Demo';
 import AllRoundEditor from './AllRoundEditor';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import './App.css';
 
 function App() {
   return (
