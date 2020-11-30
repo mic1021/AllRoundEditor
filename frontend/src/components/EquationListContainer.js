d3bb018f8619d9c3ab89ec4185ed870d27a31f31
HJ <hjkim0822@gmail.com>
2020-11-30 17:03:46 +0900

    Update EquationListContainer.js

--- frontend/src/components/EquationListContainer.js
+++ frontend/src/components/EquationListContainer.js
@@ -3,7 +3,6 @@ import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import SaveIcon from '@material-ui/icons/Save';
 import EquationList from './textAreaComponents/EquationList';
-import SavedEquations from './textAreaComponents/SavedEquations';
 import imgA from '../logo.png';
 // import { BottomNav, blah, blah } from '@material-ui/core/'; ==> problem because its going to import the whole core pkg rather than necessary ones
 
