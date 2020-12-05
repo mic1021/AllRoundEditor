7128e6ee512b768551564930c6ffdbeb121104b2
greatSumini <greatsumini@gmail.com>
2020-12-05 17:04:06 +0900

    refactor: :lipstick: add EquationListRow.Menu width

--- frontend/src/components/textAreaComponents/EquationListRow.js
+++ frontend/src/components/textAreaComponents/EquationListRow.js
@@ -8,87 +8,91 @@ import CheckBox from '@material-ui/core/Checkbox';
 import IconButton from '@material-ui/core/IconButton';
 import ArrowDropDown from '@material-ui/icons/ArrowDropDown';
 import { StaticMathField } from 'react-mathquill';
-import { TOGGLE, EDIT, DELETE, selectChecked } from '../../slices/EquationSlice';
+import {
+  TOGGLE,
+  EDIT,
+  DELETE,
+  selectChecked,
+} from '../../slices/EquationSlice';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
 
 const theme = createMuiTheme({
-    overrides: {
-      MenuItem: {
-        root:{
-        width: 'fit-content'
-        }
-      }
-    }
-  });
+  overrides: {
+    MenuItem: {
+      root: {
+        width: 'fit-content',
+      },
+    },
+  },
+});
 
 function EquationListRow(props) {
-    const [anchorEl, setAnchorEl] = React.useState(null);
+  const [anchorEl, setAnchorEl] = React.useState(null);
 
-    const checked = useSelector(selectChecked);
-    const dispatch = useDispatch();
+  const checked = useSelector(selectChecked);
+  const dispatch = useDispatch();
 
-    const handleClick = (e) => {
-        setAnchorEl(e.currentTarget);
-    }
+  const handleClick = (e) => {
+    setAnchorEl(e.currentTarget);
+  };
 
-    const handleClose = (e) => {
-        setAnchorEl(null);
-        if(e.target.textContent === 'EDIT') {
-            dispatch(EDIT(props.index));
-        }
-        if(e.target.textContent === 'DELETE') {
-            dispatch(DELETE(props.index));
-        }
+  const handleClose = (e) => {
+    setAnchorEl(null);
+    if (e.target.textContent === 'EDIT') {
+      dispatch(EDIT(props.index));
     }
-    
-    const toggleChecked = (e) => {
-        dispatch(TOGGLE(props.index));
+    if (e.target.textContent === 'DELETE') {
+      dispatch(DELETE(props.index));
     }
-     
-    return (
-        <ListItem
-            key={props.index}
-            role={undefined}
-            dense
-            button
-            onClick={toggleChecked}
+  };
+
+  const toggleChecked = (e) => {
+    dispatch(TOGGLE(props.index));
+  };
+
+  return (
+    <ListItem
+      key={props.index}
+      role={undefined}
+      dense
+      button
+      onClick={toggleChecked}
+    >
+      <ListItemIcon>
+        <CheckBox
+          edge='start'
+          checked={
+            checked[props.index] === undefined ? false : checked[props.index]
+          }
+          tabIndex='-1'
+          disableRipple
+        ></CheckBox>
+      </ListItemIcon>
+      <StaticMathField>{props.equation}</StaticMathField>
+      <ListItemSecondaryAction>
+        <IconButton aria-haspopup='true' onClick={handleClick}>
+          <ArrowDropDown />
+        </IconButton>
+        <Menu
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          keepMounted
+          onClose={handleClose}
+          style={{
+            width: 150,
+          }}
         >
-            <ListItemIcon>
-                <CheckBox
-                    edge="start"
-                    checked={checked[props.index]===undefined?false:checked[props.index]}
-                    tabIndex="-1"
-                    disableRipple
-                >
-                </CheckBox>
-            </ListItemIcon>
-            <StaticMathField>{props.equation}</StaticMathField>
-            <ListItemSecondaryAction>
-                <IconButton
-                    aria-haspopup="true"
-                    onClick={handleClick}
-                >
-                    <ArrowDropDown></ArrowDropDown>
-                </IconButton>
-                <Menu
-                    anchorEl={anchorEl}
-                    open={Boolean(anchorEl)}
-                    keepMounted
-                    onClose={handleClose}
-                >
-                <ThemeProvider theme={theme}>
-                
-                    <MenuItem onClick={handleClose}>EDIT</MenuItem>
-                    <MenuItem onClick={handleClose}>DELETE</MenuItem>
-                                  
-                </ThemeProvider>
-                </Menu>
-            </ListItemSecondaryAction>
-        </ListItem>
-    )
-};
+          <ThemeProvider theme={theme}>
+            <MenuItem onClick={handleClose}>EDIT</MenuItem>
+            <MenuItem onClick={handleClose}>DELETE</MenuItem>
+          </ThemeProvider>
+        </Menu>
+      </ListItemSecondaryAction>
+    </ListItem>
+  );
+}
 
 export default EquationListRow;
